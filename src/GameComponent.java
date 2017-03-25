c32ff0be54d932320085a44aeb593d151935ffae
coolhans <cool_hans93@hotmail.com>
2017-03-25 14:47:46 +0100

    add if paint

--- src/GameComponent.java
+++ src/GameComponent.java
@@ -13,45 +13,6 @@ public class GameComponent extends JComponent implements BoardListener {
     public GameComponent(Board board) {
         this.board = board;
 
-        EnumMap<PieceType, ImageIcon> enumMapWhite =  new EnumMap<PieceType, ImageIcon>(PieceType.class);
-        enumMapWhite.put(PieceType.PAWN, new ImageIcon("/images/wPawn.png"));
-        enumMapWhite.put(PieceType.ROOK, new ImageIcon("/images/wRook.png"));
-        enumMapWhite.put(PieceType.KNIGHT, new ImageIcon("/images/wKnight.png"));
-        enumMapWhite.put(PieceType.BISHOP, new ImageIcon("/images/wBishop.png"));
-        enumMapWhite.put(PieceType.QUEEN, new ImageIcon("/images/wQueen.png"));
-        enumMapWhite.put(PieceType.KING, new ImageIcon("/images/wKing.png"));
-       /* enumMap.put(PieceType.BPAWN, new ImageIcon("/images/Pawn.png"));
-        enumMap.put(PieceType.BROOK, new ImageIcon("/images/Rook.png"));
-        enumMap.put(PieceType.BKNIGHT, new ImageIcon("/images/Knight.png"));
-        enumMap.put(PieceType.BBISHOP, new ImageIcon("/images/Bishop.png"));
-        enumMap.put(PieceType.BQUEEN, new ImageIcon("/images/Queen.png"));
-        enumMap.put(PieceType.BKING, new ImageIcon("/images/King.png"));
-        */
-        enumMapWhite.put(PieceType.NONE, new ImageIcon("/images/Blank.jpg"));//image not used, avoid error
-        enumMapWhite.put(PieceType.OUTSIDE, new ImageIcon("/images/Blank.jpg"));//image not used, avoid error
-
-        //this.enumMap = enumMap;
-
-        EnumMap<PieceType, ImageIcon> enumMapBlack =  new EnumMap<PieceType, ImageIcon>(PieceType.class);
-        enumMapBlack.put(PieceType.PAWN, new ImageIcon("/images/Pawn.png"));
-        enumMapBlack.put(PieceType.ROOK, new ImageIcon("/images/Rook.png"));
-        enumMapBlack.put(PieceType.KNIGHT, new ImageIcon("/images/Knight.png"));
-        enumMapBlack.put(PieceType.BISHOP, new ImageIcon("/images/Bishop.png"));
-        enumMapBlack.put(PieceType.QUEEN, new ImageIcon("/images/Queen.png"));
-        enumMapBlack.put(PieceType.KING, new ImageIcon("/images/King.png"));
-        enumMapBlack.put(PieceType.NONE, new ImageIcon("/images/Blank.jpg"));//image not used, avoid error
-        enumMapBlack.put(PieceType.OUTSIDE, new ImageIcon("/images/Blank.jpg"));//image not used, avoid error
-
-        EnumMap<PieceType, ImageIcon> enumMapNoColor =  new EnumMap<PieceType, ImageIcon>(PieceType.class);
-        enumMapBlack.put(PieceType.PAWN, new ImageIcon("/images/Blank.jpg"));
-        enumMapBlack.put(PieceType.ROOK, new ImageIcon("/images/Blank.jpg"));
-        enumMapBlack.put(PieceType.KNIGHT, new ImageIcon("/images/Blank.jpg"));
-        enumMapBlack.put(PieceType.BISHOP, new ImageIcon("/images/Blank.jpg"));
-        enumMapBlack.put(PieceType.QUEEN, new ImageIcon("/images/Blank.jpg"));
-        enumMapBlack.put(PieceType.KING, new ImageIcon("/images/Blank.jpg"));
-
-        enumMapBlack.put(PieceType.NONE, new ImageIcon("/images/Blank.jpg"));//image not used, avoid error
-        enumMapBlack.put(PieceType.OUTSIDE, new ImageIcon("/images/Blank.jpg"));//image not used, avoid error
 }
 
 
@@ -139,39 +100,6 @@ public class GameComponent extends JComponent implements BoardListener {
                 img.paintIcon(this, g, (x * 60), (y * 60));
             }
         }
-        /*
-//------Paint the pieces correspondingly, including OUTSIDE.
-        for (int y = 0; y < board.getHeight(); y++) {
-            for (int x = 0; x < board.getWidth(); x++) {
-                Piece piece = board.getPiece(x, y);
-
-                if(board.getPiece(x,y).getPieceColor()==PieceColor.WHITE){
-                    this.img = enumMapWhite.get(piece);
-                }
-
-                else if(board.getPiece(x,y).getPieceColor()==PieceColor.BLACK){
-                    this.img = enumMapBlack.get(piece);
-                }
-
-                else if(board.getPiece(x,y).getPieceColor()==PieceColor.NOCOLOR){
-                    this.img = enumMapNoColor.get(piece);
-                }
-            if (piece != null) {
-                if (piece.getPieceType() == PieceType.OUTSIDE) {
-                    g.setColor(Color.DARK_GRAY);
-                    g.fillRect(y * 60, x * 60, 60, 60);
-                    //img.paintIcon(this, g, (x * 60), (y * 60));
-                }
-                else {
-                    //g.setColor(Color.GREEN);
-                    //g.fillRect(y * 50, x * 50, 40, 40);
-                    if (piece.getPieceType() != PieceType.NONE) {
-                        img.paintIcon(this, g, (x * 60), (y * 60));
-                    }
-                }
-            }
-        }
-        }*/
     }
 
     @Override
